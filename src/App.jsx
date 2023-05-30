64a4bdabef1c8d16d941abce7234b5f0ebdb8217
mg <amg.vergara@gmail.com>
2023-05-30 11:53:45 -0400

    modificacion del home

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+	<h2>Desafio marketplace mayo 2023 </h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
