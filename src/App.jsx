a8456505d363f2f7aa4b752d200326c078c25c4d
igcabrera <icabrera.illanes@gmail.com>
2023-06-07 23:11:09 -0400

    estructura dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { useState } from 'react';
 import Home from './pages/home/Home';
 import Header from './components/Header/Header';
@@ -14,6 +14,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import CarritoContext from "./context/CarritoContext";
 import Contact from './pages/contact/Contact';
 import Team from './pages/team/Team';
+import DashBoard from './pages/dashBoard/DashBoard';
 
 
 export default function App() {
@@ -32,7 +33,8 @@ export default function App() {
 
   const sharedCarritoContextState = { cartItems, setCartItems, addToCart, removeFromCart };
 
- 
+  const location = useLocation();
+  const isDashboard = location.pathname === '/dashboard';
 
   return (
     <div className="flex flex-col min-h-screen">
@@ -40,23 +42,21 @@ export default function App() {
       <ToastContainer />
 
       <BrowserRouter>
-        <Header cartItemCount={cartItems.length} />
-        <div className="grow">
-          <Routes>
-            <Route path="/" element={<Home addToCart={addToCart} />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/team" element={<Team />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/product/:id" element={<ProductDetail products={products} />} />
-            <Route
-              path="/cart"
-              element={<Cart />}
-            />
-          </Routes>
-        </div>
-        <Footer className="mt-auto" />
-      </BrowserRouter>
+          {!isDashboard && <Header cartItemCount={cartItems.length} />}
+          <div className="grow">
+            <Routes>
+              <Route path="/" element={<Home addToCart={addToCart} />} />
+              <Route path="/dashboard" element={<DashBoard />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/product/:id" element={<ProductDetail products={products} />} />
+              <Route path="/cart" element={<Cart />} />
+            </Routes>
+          </div>
+          {!isDashboard && <Footer className="mt-auto" />}
+        </BrowserRouter>
          </CarritoContext.Provider>  
     </div>
   );
