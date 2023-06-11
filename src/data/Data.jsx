de1f4b32d17dd981ba358e6251b24e0c2073e0dc
igcabrera <icabrera.illanes@gmail.com>
2023-06-11 16:50:23 -0400

    actualizar banners

--- src/data/Data.jsx
+++ src/data/Data.jsx
@@ -1,11 +1,9 @@
 const imageUrls = [
-    './IMG_PRUEBA/banner_principal_02.jpg',
-    './IMG_PRUEBA/banner_principal_03.jpg',
+    './IMG_PRUEBA/03.jpg',
+    './IMG_PRUEBA/05.jpg',
 ];
 const imageAds = [
-    'https://picsum.photos/1907/550',
-    'https://picsum.photos/1909/550',
-    'https://picsum.photos/1920/550',
+    './IMG_PRUEBA/04.jpg',
 ];
 
 
