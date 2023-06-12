a70229dddb9664aa3bf368fc6a9c0f892bdc8b7b
igcabrera <icabrera.illanes@gmail.com>
2023-06-11 23:34:35 -0400

    cambios url img

--- src/data/Data.jsx
+++ src/data/Data.jsx
@@ -1,9 +1,9 @@
 const imageUrls = [
-    './IMG_PRUEBA/03.jpg',
-    './IMG_PRUEBA/05.jpg',
+    'https://firebasestorage.googleapis.com/v0/b/proyecto-final-desafio-latam.appspot.com/o/banner_principal%2F03.jpg?alt=media&token=69c5497b-7b74-49d6-ae7a-816ec1f61b41&_gl=1*1e11s7w*_ga*MjEyMzIzMjg0Mi4xNjg2MDE3NzE4*_ga_CW55HF8NVT*MTY4NjUyMTkxNy41LjEuMTY4NjUyMjEwNC4wLjAuMA..',
+    'https://firebasestorage.googleapis.com/v0/b/proyecto-final-desafio-latam.appspot.com/o/banner_principal%2F05.jpg?alt=media&token=8f97688f-f8e6-4024-9764-a73928b13eed&_gl=1*cvgke9*_ga*MjEyMzIzMjg0Mi4xNjg2MDE3NzE4*_ga_CW55HF8NVT*MTY4NjUyMTkxNy41LjEuMTY4NjUyMjExMy4wLjAuMA..',
 ];
 const imageAds = [
-    './IMG_PRUEBA/04.jpg',
+    'https://firebasestorage.googleapis.com/v0/b/proyecto-final-desafio-latam.appspot.com/o/banner_principal%2F04.jpg?alt=media&token=9f5cffa5-b126-47ec-841b-09c6953ddc9b&_gl=1*kmj02a*_ga*MjEyMzIzMjg0Mi4xNjg2MDE3NzE4*_ga_CW55HF8NVT*MTY4NjUyMTkxNy41LjEuMTY4NjUyMjExMS4wLjAuMA..',
 ];
 
 
